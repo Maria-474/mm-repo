fc49bef320b4f7125ef8cc72185dc8e979284f6a
Maria <mariamatveeva474@gmail.com>
2026-03-24 16:06:04 +0300

    fix(vite): base link

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/maria-474.github.io/',
+  base: '/maria-474.github.io/mm-repo',
   plugins: [react()],
   resolve: {
     alias: {
